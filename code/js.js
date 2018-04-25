a6e703c5d26027f140c67041f5c045c5089c59dd
AwesomeTy79 <32660892+AwesomeTy79@users.noreply.github.com>
2018-04-24 20:48:30 -0600

    Update js.js

--- code/js.js
+++ code/js.js
@@ -8,3 +8,13 @@ async function factClicked() {
     await sleep(100);
     $('h1#fact').css({"color":"white","text-decoration":"none"})
 }
+function getParameterByName(name, url) {
+    if (!url) {url = window.location.href;}
+    var name = name.replace('/[\[\]]/g', "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) {return null;}
+    if (!results[2]) {return '';}
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+(function () { var sayhi = getParameterByName('sayhi'); if (sayhi != null || sayhi != false) { alert('hi'); } }());
