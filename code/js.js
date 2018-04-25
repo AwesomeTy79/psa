243ec5056bc423270a649dae95cbfdfd7d0f5320
AwesomeTy79 <32660892+AwesomeTy79@users.noreply.github.com>
2018-04-24 21:18:39 -0600

    Update js.js

--- code/js.js
+++ code/js.js
@@ -8,13 +8,22 @@ async function factClicked() {
     await sleep(100);
     $('h1#fact').css({"color":"white","text-decoration":"none"})
 }
-function getParameterByName(name, url) {
-    if (!url) {url = window.location.href;}
-    var name = name.replace('/[\[\]]/g', "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) {return null;}
-    if (!results[2]) {return '';}
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
+var urlParams;
+(window.onpopstate = function () {
+    var match,
+        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        search = /([^&=]+)=?([^&]*)/g,
+        decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+        query  = window.location.search.substring(1);
+
+    urlParams = {};
+    while (match = search.exec(query))
+       urlParams[decode(match[1])] = decode(match[2]);
+})(); 
+function sayhi(variable) {
+	if (typeof variable !== 'undefined') {
+    	
+    	alert('hi');
+	}
 }
-(function () { var sayhi = getParameterByName('sayhi'); if (sayhi != null && sayhi != false) { alert('hi'); } }());
+sayhi(urlParams['sayhi']);
